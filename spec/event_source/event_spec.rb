61a345b171daeea67cd8db2845a224dd0deefe53
Dan Thomas <dan@ideacrew.com>
2021-02-17 13:25:57 -0500

    Add pseudo code comments

--- spec/event_source/event_spec.rb
+++ spec/event_source/event_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
+# 0. EventSource Gem supports domain model entities
+# 1. Command
+# 1.1 We will use Operations as ES Commands by doing: include EventSource::Events
+# 1.2 An operation can have 1 or more Events
+# 1.3 EventSource::Events builds events and fires event upon success of command
+# 2. Event
+# 2.1 Events are predefined and include attributes and validation using Dry Hash Schema & Dry Validation
+# 3. Dispatcher
+# 3.1 Use RuleSet and ListenerSet to register observers and distribute events
+# 4. EventStream
+# 4.1 Future: v0.3.0 will not include persistence
+
 RSpec.describe EventSource::Events do
   context 'it should do something useful' do
   end
