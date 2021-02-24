0f035a32ed8eb9995797a53e22bd89b39725039b
Dan Thomas <dan@ideacrew.com>
2021-02-24 11:29:11 -0500

    Add constant name generation to Publisher module

--- spec/rails_app/app/event_source/publishers/publisher.rb
+++ spec/rails_app/app/event_source/publishers/publisher.rb
@@ -1,12 +1,16 @@
 module Publishers
   module Publisher
-    # Find all the publishers
+    # Find all defined publishers, instantiante an instance and assiggn to constant
+    # @example
+    #   File: organization_publisher.rb => ORGANIZATION_PUBLISHER = OrganizationPublisher.new
+    #   File: parties/organization_publisher.rb => PARTIES_ORGANIZATION_PUBLISHER = Parties::OrganizationPublisher.new
     def register_publishers
       publisher_root = Pathname(__FILE__).dirname
-      Dir[publisher_root.join('**', '*_publisher.rb')].each do |file|
-        binding.pry
 
-        # ORGANIZATION_PUBLISHER = OrganizationPublisher.new
+      Dir[publisher_root.join('**', '*_publisher.rb')].each do |file|
+        constant_name =
+          file.split('/').each { |f| f.upcase! }.join('::').chomp!('.RB')
+        klass_name = '' # TODO need titlecase-like function
       end
     end
   end
