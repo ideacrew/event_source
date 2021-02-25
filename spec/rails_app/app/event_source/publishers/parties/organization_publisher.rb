a420a6e0122938ea51965d569fd3fd20b6d523fd
Dan Thomas <dan@ideacrew.com>
2021-02-24 21:38:57 -0500

    Add documentation

--- spec/rails_app/app/event_source/publishers/parties/organization_publisher.rb
+++ spec/rails_app/app/event_source/publishers/parties/organization_publisher.rb
@@ -7,6 +7,7 @@ module Parties
   class OrganizationPublisher
     include Dry::Events::Publisher[:organization_publisher]
 
+    # Subscribers may register for block events directly in publisher class
     register_event 'parties.organization.created'
     register_event 'parties.organization.fein_corrected'
     register_event 'parties.organization.fein_updated'
@@ -14,6 +15,7 @@ module Parties
     subscribe('organization.created') { |event| puts '---Hello world!!' }
   end
 
+  # Subscribers may register for non-block events in publisher instance
   ORGANIZATION_PUBLISHER = OrganizationPublisher.new
   # ORGANIZATION_PUBLISHER.subscribe(
   #   Organizations::OrganizationCreatedListener.new
