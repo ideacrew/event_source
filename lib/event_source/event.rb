01580182373acdc38fa47a09e84a47582eaa6cd9
Dan Thomas <dan@ideacrew.com>
2021-05-10 15:09:01 -0400

    Add API doc and update spec

--- lib/event_source/event.rb
+++ lib/event_source/event.rb
@@ -1,21 +1,16 @@
 # frozen_string_literal: true
 
-# require 'dry/events/publisher'
-
 module EventSource
-  # A notification that something has happened in the system
-  # @example
-  # An Event has the following public API
-  #   MyEvent.call(event_key, options)
-  #   event = MyEvnet.new(event_key, options:)
+  # A class for generating a notification that something has happened in the system
   #
-  # (attributes:, metadata:, contract_key:)
+  # @attr_reader [Array<String>] attribute_keys optional list of attributes that must be included in { Payload }
+  # @attr_reader [String] publisher_key namespaced key indicating the class that registers event for publishing
+  # @attr_reader [String] event_key the name of the event instance
+  # @attr_reader [String] payload attribute/value pairs for the message that accompanies the event
   #
-  #   event.valid? # true or false
-  #   event.errors # +> <Dry::Validation::Errors ... >
-  #   event.publish # validate and execute the command
   class Event
     extend Dry::Initializer
+    attr_reader :attribute_keys, :publisher_key, :event_key, :payload
 
     HeaderDefaults = {
       version: '3.0',
@@ -25,8 +20,82 @@ module EventSource
       # entity_kind: ''
     }.freeze
 
-    class << self
+    def initialize(options = {})
+      @attribute_keys = klass_var_for(:attribute_keys) || []
+      @payload = {}
 
+      send(:payload=, options[:attributes] || {})
+
+      metadata = (options[:metadata] || {}).merge(event_key: event_key)
+
+      # @publisher_key = klass_var_for(:publisher_key) || nil
+      # raise EventSource::Error::PublisherKeyMissing, "add 'publisher_key' to #{self.class.name}" if @publisher_key.eql?(nil)
+    end
+
+    # Set payload
+    # @overload payload=(payload)
+    #   @param [Hash] payload New payload
+    #   @return [Event] A copy of the event with the provided payload
+    def payload=(values)
+      unless values.instance_of?(Hash)
+        raise ArgumentError, 'payload must be a hash'
+      end
+
+      values.symbolize_keys!
+
+      @payload =
+        values.select do |key, _value|
+          attribute_keys.empty? || attribute_keys.include?(key)
+        end
+
+      validate_attribute_presence
+    end
+
+    # Verify an event instance has no errors and may be published
+    # @return [Boolean]
+    def valid?
+      event_errors.empty?
+    end
+
+    # Send the event instance to its producer so that it may be accessed by subscribers
+    # @raise [EventSource::Error::AttributesInvalid]
+    def publish
+      raise EventSource::Error::AttributesInvalid, @event_errors unless valid?
+
+      # EventSource.adapter.enqueue(self)
+      EventSource.adapter.publish(event_key, payload)
+    end
+
+    def event_key
+      return @event_key if defined?(@event_key)
+      @event_key = self.class.name.gsub('::', '.').underscore
+    end
+
+    def event_errors
+      @event_errors ||= []
+    end
+
+    # Coerce an event to a hash
+    # @return [Hash]
+    def to_h
+      @payload
+    end
+
+    # Get payload attribute values
+    # @param [String, Symbol] name
+    def [](name)
+      payload[name]
+    end
+
+    # Set payload attribute values
+    def []=(name, value)
+      @payload.merge!({ "#{name}": value })
+      validate_attribute_presence
+      self[name]
+    end
+
+    # Class methods
+    class << self
       def publisher_key(value = nil)
         set_instance_variable_for(:publisher_key, value)
       end
@@ -46,91 +115,13 @@ module EventSource
 
       def set_instance_variable_for(element, value)
         if value.nil?
-          return instance_variable_get("@#{element}") if instance_variable_defined?("@#{element}")
+          if instance_variable_defined?("@#{element}")
+            return instance_variable_get("@#{element}")
+          end
         else
           instance_variable_set("@#{element}", value)
         end
       end
-    end
-
-    # @!attribute [r] id
-    # @return [Symbol, String] The event identifier
-    # attr_accessor :attributes
-    attr_reader :attribute_keys,
-                :publisher_key,
-                :publisher_class,
-                :headers,
-                :payload
-
-    def initialize(options = {})
-      @attribute_keys = klass_var_for(:attribute_keys) || []
-
-      @payload = {}
-      send(:payload=, options[:attributes] || {})
-
-      metadata = (options[:metadata] || {}).merge(event_key: event_key)
-      @headers = HeaderDefaults.merge(metadata)
-
-      # @publisher_key = klass_var_for(:publisher_key) || nil
-      # raise EventSource::Error::PublisherKeyMissing, "add 'publisher_key' to #{self.class.name}" if @publisher_key.eql?(nil)
-      # TODO: Verify if needed
-      # @publisher_class = constant_for(@publisher_key)
-    end
-
-    # Set payload
-    # @overload payload=(payload)
-    #   @param [Hash] payload New payload
-    #   @return [Event] A copy of the event with the provided payload
-
-    def payload=(values)
-      raise ArgumentError, 'payload must be a hash' unless values.instance_of?(Hash)
-
-      values.symbolize_keys!
-
-      @payload =
-        values.select do |key, _value|
-          attribute_keys.empty? || attribute_keys.include?(key)
-        end
-
-      validate_attribute_presence
-    end
-
-    # @return [Boolean]
-    def valid?
-      event_errors.empty?
-    end
-
-    def publish
-      raise EventSource::Error::AttributesInvalid, @event_errors unless valid?
-      # EventSource.adapter.enqueue(self)
-      EventSource.adapter.publish(event_key, payload)
-    end
-
-    def event_key
-      return @event_key if defined? @event_key
-      @event_key = self.class.name.gsub('::', '.').underscore
-    end
-
-    def event_errors
-      @event_errors ||= []
-    end
-
-    # Coerce an event to a hash
-    # @return [Hash]
-    def to_h
-      @payload
-    end
-
-    # Get data from the payload
-    # @param [String, Symbol] name
-    def [](name)
-      payload[name]
-    end
-
-    def []=(name, value)
-      @payload.merge!({ "#{name}": value })
-      validate_attribute_presence
-      self[name]
     end
 
     private
@@ -139,13 +130,16 @@ module EventSource
       return unless attribute_keys.present?
       gapped_keys = attribute_keys - payload.keys
       @event_errors = []
-      event_errors.push("missing required keys: #{gapped_keys}") unless gapped_keys.empty?
+      unless gapped_keys.empty?
+        event_errors.push("missing required keys: #{gapped_keys}")
+      end
     end
 
     def constant_for(value)
       constant_name = value.split('.').each(&:upcase!).join('_')
       return constant_name.constantize if Object.const_defined?(constant_name)
-      raise EventSource::Error::ConstantNotDefined, "Constant not defined for: '#{constant_name}'"
+      raise EventSource::Error::ConstantNotDefined,
+            "Constant not defined for: '#{constant_name}'"
     end
 
     def klass_var_for(var_name)
--- spec/event_source/event_spec.rb
+++ spec/event_source/event_spec.rb
@@ -68,21 +68,16 @@ RSpec.describe EventSource::Event do
     # end
 
     context 'and the required publisher_key provided is valid' do
-      let(:valid_event_class) do
-        EventSource::MyValidEvent
-      end
+      let(:valid_event_class) { EventSource::MyValidEvent }
 
       subject { valid_event_class.new }
+
       # it 'should initialize without error' do
       #   expect(subject.publisher_class).to be_a(Parties::OrganizationPublisher)
       # end
 
-      it 'should have default metadata values' do
-        metadata = subject.headers
-
-        expect(metadata[:version]).to be_present
-        expect(metadata[:event_key]).to be_present
-        # expect(metadata[:created_at]).to be_present
+      it 'should have an event_key' do
+        expect(subject.event_key).to eq 'event_source.my_valid_event'
       end
     end
 
@@ -94,9 +89,7 @@ RSpec.describe EventSource::Event do
   end
 
   context 'An initialized Event class with defined attribute_keys' do
-    let(:event_class) do
-      EventSource::MyEvent
-    end
+    let(:event_class) { EventSource::MyEvent }
 
     it 'keys should be initialized for each attribute' do
       expect(event_class.new.attribute_keys).to eq %i[hbx_id fein entity_kind]
@@ -139,17 +132,13 @@ RSpec.describe EventSource::Event do
       it 'should ignore extra attributes' do
         extra_keys = attributes.keys - subject.attribute_keys
 
-        extra_keys.each do |key|
-          expect(subject.payload.key?(key)).to be_falsey
-        end
+        extra_keys.each { |key| expect(subject.payload.key?(key)).to be_falsey }
       end
     end
   end
 
   context 'An initialized Event class with no attribute_keys' do
-    let(:event_class) do
-      EventSource::MyEventTwo
-    end
+    let(:event_class) { EventSource::MyEventTwo }
 
     subject { event_class.new }
     it 'attribute_keys should be empty' do
@@ -195,9 +184,7 @@ RSpec.describe EventSource::Event do
   end
 
   context 'An initialized Event class with attribute_keys' do
-    let(:event_class) do
-      EventSource::MyEventThree
-    end
+    let(:event_class) { EventSource::MyEventThree }
 
     context 'with attributes passed' do
       let(:attributes) do
@@ -213,11 +200,11 @@ RSpec.describe EventSource::Event do
 
       it 'attribute_keys should be present' do
         expect(subject.attribute_keys).to eq %i[
-          hbx_id
-          entity_kind
-          fein
-          legal_name
-        ]
+             hbx_id
+             entity_kind
+             fein
+             legal_name
+           ]
       end
       it '#event_errors should be empty' do
         expect(subject.event_errors).to be_empty
@@ -231,20 +218,11 @@ RSpec.describe EventSource::Event do
     end
 
     context 'with attribute setter' do
-      let(:attributes) do
-        {
-          hbx_id: '553234',
-          fein: '546232323'
-        }
-      end
+      let(:attributes) { { hbx_id: '553234', fein: '546232323' } }
 
-      let(:metadata) do
-        { event_key: 'parties.organization.created' }
-      end
+      let(:metadata) { { event_key: 'parties.organization.created' } }
 
-      subject do
-        event_class.new(attributes: attributes, metadata: metadata)
-      end
+      subject { event_class.new(attributes: attributes, metadata: metadata) }
 
       context 'when a name value pair is passed' do
         let(:legal_name) { 'Test Corp LLC' }
@@ -259,17 +237,11 @@ RSpec.describe EventSource::Event do
     end
 
     context 'with attribute getter' do
-      let(:attributes) do
-        {
-          hbx_id: '553234',
-          fein: '546232323'
-        }
-      end
+      let(:attributes) { { hbx_id: '553234', fein: '546232323' } }
 
       subject { event_class.new(attributes: attributes) }
 
       context 'when attribute name is passed' do
-
         it 'should return the value' do
           expect(subject[:fein]).to eq attributes[:fein]
           expect(subject[:hbx_id]).to eq attributes[:hbx_id]
