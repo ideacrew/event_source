c0bab7f849e34c1ea48ae6c4210d9c1d0b9a75ff
Dan Thomas <dan@ideacrew.com>
2021-05-11 14:40:06 -0400

    Udate API documentation

--- lib/event_source/publisher.rb
+++ lib/event_source/publisher.rb
@@ -4,9 +4,9 @@ require 'dry/inflector'
 require 'concurrent/map'
 
 module EventSource
-  # Publisher mixin to register events and create channels
+  # Mixin to register and publish Events
   class Publisher < Module
-    include Dry::Equalizer(:id)
+    include Dry.Equalizer(:id)
 
     # Internal publisher registry, which is used to identify them globally
     #
@@ -54,7 +54,7 @@ module EventSource
       # end
 
       def register_event(event_key, options = {})
-        @events = {} unless defined? @events
+        @events = {} unless defined?(@events)
         @events[event_key] = options
         self
       end
@@ -63,7 +63,11 @@ module EventSource
         # FIXME: .key works only for Ruby 1.9 or later
         publisher_key = EventSource::Publisher.publisher_container.key(self)
         events.each do |event_key, options|
-          EventSource.connection.create_channel(publisher_key, event_key, options)
+          EventSource.connection.create_channel(
+            publisher_key,
+            event_key,
+            options
+          )
         end
       end
     end
