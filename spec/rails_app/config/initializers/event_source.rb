31f60189a7408a254931a73c94960cef72a14057
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2021-06-14 09:40:08 -0400

    Refs #Gems, downgrade bunny & nokogiri dependencies

--- spec/rails_app/config/initializers/event_source.rb
+++ spec/rails_app/config/initializers/event_source.rb
@@ -37,7 +37,7 @@ EventSource.configure do |config|
     # - RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-guest}
     server.amqp do |rabbitmq|
       rabbitmq.host = "amqp://localhost" # ENV['RABBITMQ_HOST']
-      rabbitmq.vhost =  "event_source" # ENV['RABBITMQ_VHOST']
+      rabbitmq.vhost =  "/" # ENV['RABBITMQ_VHOST']
       rabbitmq.port = "5672" # ENV['RABBITMQ_PORT']
       rabbitmq.url = "" # ENV['RABBITMQ_URL']
       rabbitmq.user_name = "" # ENV['RABBITMQ_USERNAME']
