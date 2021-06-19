c88608f3dc78c61e58e510109238d4822fe8683e
Dan Thomas <dan@ideacrew.com>
2021-06-19 16:53:51 -0400

    WIP on adding status to HTTP response

---
+++ spec/event_source/worker_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe EventSource::Worker do
+  let(:config) { { num_threads: 2 } }
+  let(:queue_proxy) {}
+  let(:response) {}
+
+  context '' do
+    expect
+  end
+end
