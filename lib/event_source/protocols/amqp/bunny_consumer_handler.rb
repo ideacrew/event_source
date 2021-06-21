5698486451f3e4aab5cfc75db6f72c4b731d2eab
Dan Thomas <dan@ideacrew.com>
2021-06-21 15:31:58 -0400

    Move BunnyConsumerHandler into AMQP namespace

---
+++ lib/event_source/protocols/amqp/bunny_consumer_handler.rb
@@ -0,0 +1,47 @@
+module EventSource
+  module Protocols
+    module Amqp
+      class BunnyConsumerHandler
+        attr_reader :subscriber
+        include EventSource::Logging
+
+        def initialize(
+          subscriber,
+          delivery_info,
+          metadata,
+          payload,
+          &executable
+        )
+          @subscriber = subscriber
+          @delivery_info = delivery_info
+          @metadata = metadata
+          @payload = payload
+          @executable = executable
+        end
+
+        def run
+          subscriber.instance_exec(
+            @delivery_info,
+            @metadata,
+            @payload,
+            &@executable
+          )
+          callbacks.fetch(:on_success).call
+        rescue StandardError => e
+          callbacks.fetch(:on_failure).call(e.backtrace)
+        end
+
+        def callbacks
+          {
+            on_success:
+              lambda { logger.debug 'Subscription executed successfully!!' },
+            on_failure:
+              lambda do |exception|
+                logger.error "Subscription execution failed due to exception: #{exception}"
+              end
+          }
+        end
+      end
+    end
+  end
+end
