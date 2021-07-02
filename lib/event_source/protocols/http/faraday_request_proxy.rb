b941d2b88ccc978d295ae8c574d6020d2b04b7b1
Trey Evans <lewis.r.evans@gmail.com>
2021-07-02 10:48:29 -0400

    Remove path aliasing.

--- lib/event_source/protocols/http/faraday_request_proxy.rb
+++ lib/event_source/protocols/http/faraday_request_proxy.rb
@@ -106,8 +106,7 @@ module EventSource
         end
 
         def request_path
-          connection_request_path = connection_proxy.request_path
-          connection_request_path.blank? ? channel_proxy.name : connection_request_path
+          channel_proxy.name
         end
 
         def faraday_request_for(bindings)
